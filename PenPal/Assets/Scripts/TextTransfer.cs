ff0ec5fffacf047da4e74ccdf71d76de430c1e1e
Sailesh Dahal <sdahal@knox.edu>
2020-05-02 21:25:52 -0500

    Letter Screen Basic Functionality

---
+++ PenPal/Assets/Scripts/TextTransfer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTransfer : MonoBehaviour
+{
+    public string text;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void StoreLetter(){
+        text = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = text;
+    }
+
+    public void clearText(){
+        textDisplay.GetComponent<Text>().text = "";
+    }
+}
