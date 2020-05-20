3aa9d8dfe284210cfeb31f117d735cc11c1a9376
Max Warren <mlwarren@knox.edu>
2020-05-20 12:52:32 -0400

    Profile Screen
    Added some png images also made changes to the design of the profile screen. Added scripts to implement a tab system, but we will probably opt out of that due to spacing concerns. So this is all subject to change.

---
+++ PenPal/Assets/TabButton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+//This requires us to have an image component on our buttons
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public TabGroup tabGroup;
+    public Image background;
+    public UnityEvent onTabSelected;
+    public UnityEvent onTabDeselected;
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
+    }
+
+    public void Selected()
+    {
+        if(onTabSelected != null)
+        {
+            onTabSelected.Invoke();
+        }
+    }
+
+    public void Deselect()
+    {
+        if(onTabDeselected != null)
+        {
+            onTabDeselected.Invoke();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        background = GetComponent<Image>();
+        tabGroup.Subscribe(this); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
