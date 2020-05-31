b3cc358ba967cd68e2e4d0af2173637b253cf8e0
Max Warren <mlwarren@knox.edu>
2020-05-31 13:53:20 -0400

    Json Changes
    Working Json implemented...more changes to come

---
+++ PenPal/Assets/Scripts/UserData/PersonalData.cs
@@ -0,0 +1,13 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PersonalData 
+{
+    public string userName = "Max Warren";
+
+    public string userBio = "I grew up in the suburbs of Chicago";
+
+    public string userInterests = "I enjoy playing soccer, watching anime, and reading comic books";
+}
